6d154dc84efde1e11acb2b06f1b12aa3eff9a7b7
Mike Dacanay <migd64@gmail.com>
2017-10-04 16:17:35 -0700

    final commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates :first_name, :last_name, length: { minimum: 2 },presence: true 
+	validates :email_address, presence: true
+	validates :age, presence: true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 10, :less_than_or_equal_to => 150 }
 end
